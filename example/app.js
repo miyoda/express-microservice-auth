28c83965919a96eaf0c16ff41a12a15098c8b0b1
miyomahc <miyomahc@gmail.com>
2016-11-12 15:51:44 +0100

    example port var

--- example/app.js
+++ example/app.js
@@ -1,13 +1,14 @@
 'use strict';
 
 console.log("Starting example server...");
-var port = 3000;
 
 var mongoose = require('mongoose');
 mongoose.set('debug', true);
 
 var express = require('express');
 var app = express();
+
+app.set('port', (process.env.PORT || 3000));
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -16,14 +17,14 @@ app.use(bodyParser.json());
 var expressMicroserviceAuth = require('../');
 
 var swaggerDefinition = {
-  host: 'localhost:'+port,
+  host: 'localhost:'+app.get('port'),
   basePath: '/auth/v1',
   schemes: ['http']
 }
 app.use('/auth/v1', expressMicroserviceAuth(require('./db'), swaggerDefinition, require('./options.json')));
 
 var swaggerDefinitionBis = {
-  host: 'localhost:'+port,
+  host: 'localhost:'+app.get('port'),
   basePath: '/authBis/v1',
   schemes: ['http']
 }
@@ -34,6 +35,6 @@ app.get("/", function(req, res) {
   res.status(200).send("express-microservice-auth EXAMPLE SERVER /");
 });
 
-app.listen(port, function() {
-  console.log('Listening on %s', port)
+app.listen(app.get('port'), function() {
+  console.log('Listening on %s', app.get('port'))
 });
